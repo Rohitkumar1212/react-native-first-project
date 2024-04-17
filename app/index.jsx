fa67ad2403a895ad1c0026ceb41b24b29bb95c5f
Rohitkumar1212 <1997errohitkumar@gmail.com>
2024-04-17 15:21:23 +0530

    tabs navigation done

---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import {  Text, View } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack text-red-500">Auro</Text>
+      <StatusBar style="auto" />
+      <Link href={"/home"} className="text-blue-500 text-3xl">
+        go to home
+      </Link>
+    </View>
+  );
+}
